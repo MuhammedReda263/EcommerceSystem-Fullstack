14b8ac670de45f98f2307d64b7299c505555165f
Muhammed Reda <moreda263@gmail.com>
2025-11-30 20:27:07 +0200

    add global route loading spinner using router events

--- Ecom Frontend Angular/src/app/app.ts
+++ Ecom Frontend Angular/src/app/app.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, signal } from '@angular/core';
 import { IProducts } from './shared/Models/Product';
 import { IPagination } from './shared/Models/Pagination';
 import { IdentityService } from './identity/identity-service';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { LoadingService } from './core/services/loading';
 
 @Component({
   selector: 'app-root',
@@ -11,19 +13,33 @@ import { IdentityService } from './identity/identity-service';
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
-constructor(private _identitySerivce : IdentityService){}
+  constructor(private _identitySerivce: IdentityService, private router: Router, public loading: LoadingService) { }
 
   ngOnInit(): void {
-      this._identitySerivce.loadRole();
-      this._identitySerivce.isAuth().subscribe({
-        next:(value)=> {
-          this._identitySerivce.authState.next(true);
-        },
-        error:(value)=>{
-          this._identitySerivce.authState.next(false)
-        }
-      })
+    this._identitySerivce.loadRole();
+    this._identitySerivce.isAuth().subscribe({
+      next: (value) => {
+        this._identitySerivce.authState.next(true);
+      },
+      error: (value) => {
+        this._identitySerivce.authState.next(false)
+      }
+    })
 
+
+    this.router.events.subscribe((event) => {
+
+      if (event instanceof NavigationStart) {
+        this.loading.show();
+      }
+
+      if (event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError) {
+        this.loading.hide();
+      }
+
+    });
   }
 
 }
--- Ecom Frontend Angular/src/app/core/services/loading.ts
+++ Ecom Frontend Angular/src/app/core/services/loading.ts
@@ -1,20 +1,17 @@
 import { Injectable, OnInit } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Loading implements OnInit {
-   constructor(private spinner: NgxSpinnerService) {}
+export class LoadingService {
+  public isLoading = new BehaviorSubject<boolean>(false);
 
-  ngOnInit() {
-    /** spinner starts on init */
-    this.spinner.show();
-
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide();
-    }, 5000);
+  show() {
+    this.isLoading.next(true);
   }
-  
+
+  hide() {
+    this.isLoading.next(false);
+  }
 }
